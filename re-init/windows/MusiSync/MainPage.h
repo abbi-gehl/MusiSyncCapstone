c80a7da95cd1140a29d7bf2da29f3d65d66430ae
Harrison-Blair <hjblair@me.com>
2025-02-26 20:30:59 -0500

    reinit pre mac init

---
+++ re-init/windows/MusiSync/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::MusiSync::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::MusiSync::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
