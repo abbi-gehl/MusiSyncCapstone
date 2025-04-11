0f6a7ddb5c30db2d3c4d9ceed3dfc6491bda95e6
Harrison-Blair <hjblair@me.com>
2025-04-10 22:07:40 -0400

    ChunkStore Implimentation

---
+++ db/chunkStore.tsx
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { Buffer } from 'buffer';
+
+interface ChunkState {
+    chunkStore: {
+        name: string;
+        totalChunks: number;
+        chunkArray: Buffer[];
+    } | null;
+    currentChunkSet: {
+        totalChunks: number;
+        chunkArray: Buffer[];
+    } | null;
+
+    setChunkStore: (chunkStore: any) => void;
+    resetChunkStore: () => void;
+    setCurrentChunkSet: (chunkSet: any) => void;
+    resetCurrentChunkSet: () => void;
+}
+
+export const useChunkStore = create<ChunkState>(set => ({
+    chunkStore: null,
+    currentChunkSet: null,
+    setChunkStore: chunkStore => set(() => ({ chunkStore })),
+    resetChunkStore: () => set(() => ({ chunkStore: null })),
+    setCurrentChunkSet: currentChunkSet => set(() => ({ currentChunkSet })),
+    resetCurrentChunkSet: () => set(() => ({ currentChunkSet: null })),
+}));
